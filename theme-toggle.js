f8689306346f5aaac98afcba7a497073f19c74fc
klemp0 <caliniucnikita@gmail.com>
2025-11-11 11:07:16 +0200

    added switch mode function

---
+++ theme-toggle.js
@@ -0,0 +1,17 @@
+var currentState = true;
+let changeButton = document.getElementById("themeToggle");
+
+function switchTheme() {
+    if (currentState) {
+        document.getElementById('style').href = "main.css", "header.css";
+        document.getElementById('light').style.display = "inline";
+        document.getElementById('dark').style.display = "none";
+    }else{
+        document.getElementById('style').href = "switch.css";
+        document.getElementById('dark').style.display = "inline";
+        document.getElementById('light').style.display = "none";
+    }
+    currentState = !currentState;
+}
+changeButton.addEventListener("click", switchTheme);
+
