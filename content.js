1d0a1e57a0f48903ed58f1e7b77c1bf83b200aee
klemp0 <caliniucnikita@gmail.com>
2026-02-03 11:29:38 +0200

    still not functionable

---
+++ content.js
@@ -0,0 +1,8 @@
+const nav = document.createElement("nav");
+const logo = document.createElement("div");
+logo.setAttribute("class", "logo");
+
+const br = logo.getElementsByTagName("a");
+br.setAttribute("href", "", "class", "brand");
+br.innerText ="CloathingShop";
+logo.appendChild(br);
