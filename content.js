51432fd10b4d969198cebffc662d771d4a4fa6a1
klemp0 <caliniucnikita@gmail.com>
2026-02-17 10:56:26 +0200

    am redactat structura in js

--- content.js
+++ content.js
@@ -1,326 +1,139 @@
-const head = document.getElementsByClassName('site-header')[0];
+const head = document.getElementsByClassName("site-header")[0];
 
+const topbar = document.createElement("div");
+topbar.className = "topbar";
 
-const topbar = document.createElement('div');
-topbar.setAttribute("class", "topbar");
-
-const topr = document.createElement('div');
-topr.setAttribute("class", "topbar-right top-dark" );
+const topr = document.createElement("div");
+topr.className = "topbar-right top-dark";
 
 const ahelp = document.createElement("a");
-ahelp.setAttribute("class", "a-white");
-ahelp.innerText="Help";
-ahelp.setAttribute("href", "#")
+ahelp.className = "a-white";
+ahelp.href = "#";
+ahelp.innerText = "Help";
 
-const sep = document.createElement('span');
-sep.setAttribute("class", "sep sep-white");
-sep.innerText="|";
+const sep = document.createElement("span");
+sep.className = "sep sep-white";
+sep.innerText = "|";
 
-const asign = document.createElement('a');
-asign.setAttribute("class", "a-white");
-asign.innerText="Sign In";
-asign.setAttribute("href", "#");
+const asign = document.createElement("a");
+asign.className = "a-white";
+asign.href = "#";
+asign.innerText = "Sign In";
 
+topr.append(ahelp, sep, asign);
 topbar.appendChild(topr);
-topr.appendChild(ahelp);
-topr.appendChild(sep);
-topr.appendChild(asign);
 head.appendChild(topbar);
 
-
-const nav = document.createElement('nav');
-nav.setAttribute("class", "navbar");
+const nav = document.createElement("nav");
+nav.className = "navbar";
 head.appendChild(nav);
 
-const logo = document.createElement('div');
-logo.setAttribute("class", "logo");
+const logo = document.createElement("div");
+logo.className = "logo";
 
-const abrand = document.createElement('a');
-abrand.setAttribute("class", "brand");
-abrand.innerText="Cloathing Shop";
-abrand.setAttribute("href", "#");
+const abrand = document.createElement("a");
+abrand.className = "brand";
+abrand.href = "#";
+abrand.innerText = "Clothing Shop";
 
 logo.appendChild(abrand);
 nav.appendChild(logo);
 
+const catalogData = {
+  men: {
+    Shoes: ["All Shoes", "Sneakers", "Shoes", "Sport"],
+    Clothing: ["All Clothing", "Hoodies", "Tops & T-shirts", "Accessories"],
+    Sport: ["Running", "Football", "Basketball", "Training & Gym"]
+  },
+  woman: {
+    Shoes: ["All Shoes", "Sneakers", "Shoes", "Sport"],
+    Clothing: ["All Clothing", "Hoodies", "Leggings", "Tops & T-shirts"],
+    Sport: ["Training & Gym", "Running", "Tennis", "Football"]
+  },
+  kids: {
+    Shoes: ["All Shoes", "Sneakers", "Shoes", "Sport"],
+    Clothing: ["All Clothing", "Tops & T-Shirts", "Tracksuits", "Jackets"],
+    Sport: ["Running", "Football", "Basketball", "Physical Education"]
+  }
+};
+
+const catalogSections = [
+  { title: "Men", key: "men" },
+  { title: "Woman", key: "woman" },
+  { title: "Kids", key: "kids" }
+];
 
 const catalog = document.createElement("ul");
-catalog.setAttribute("class", "catalog");
+catalog.className = "catalog";
 nav.appendChild(catalog);
 
+function createMegaMenu(data) {
+  const mega = document.createElement("div");
+  mega.className = "mega";
+  mega.setAttribute("role", "menu");
+  mega.setAttribute("aria-hidden", "true");
 
-const liMen = document.createElement("li");
-liMen.setAttribute("class", "has-mega");
+  const megaInner = document.createElement("div");
+  megaInner.className = "mega-inner";
 
-const aMen = document.createElement("a");
-aMen.setAttribute("href", "#");
-aMen.innerText = "Men";
+  Object.entries(data).forEach(([category, items]) => {
+    const col = document.createElement("div");
+    col.className = "mega-col";
 
-liMen.appendChild(aMen);
-catalog.appendChild(liMen);
+    const h5 = document.createElement("h5");
+    h5.innerText = category;
 
-const megaMen = document.createElement("div");
-megaMen.setAttribute("class", "mega");
-megaMen.setAttribute("role", "menu");
-megaMen.setAttribute("aria-hidden", "true");
+    const ul = document.createElement("ul");
 
-const megaInnerMen = document.createElement("div");
-megaInnerMen.setAttribute("class", "mega-inner");
+    items.forEach(text => {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      a.href = "#";
+      a.innerText = text;
+      li.appendChild(a);
+      ul.appendChild(li);
+    });
 
-megaMen.appendChild(megaInnerMen);
-liMen.appendChild(megaMen);
+    col.append(h5, ul);
+    megaInner.appendChild(col);
+  });
 
-const colMenShoes = document.createElement("div");
-colMenShoes.setAttribute("class", "mega-col");
+  mega.appendChild(megaInner);
+  return mega;
+}
 
-const hMenShoes = document.createElement("h5");
-hMenShoes.innerText = "Shoes";
-
-const ulMenShoes = document.createElement("ul");
-
-["All Shoes", "Sneakers", "Shoes", "Sport"].forEach(text => {
+catalogSections.forEach(section => {
   const li = document.createElement("li");
+  li.className = "has-mega";
+
   const a = document.createElement("a");
   a.href = "#";
-  a.innerText = text;
-  li.appendChild(a);
-  ulMenShoes.appendChild(li);
+  a.innerText = section.title;
+
+  li.append(a, createMegaMenu(catalogData[section.key]));
+  catalog.appendChild(li);
 });
-
-colMenShoes.append(hMenShoes, ulMenShoes);
-megaInnerMen.appendChild(colMenShoes);
-
-const colMenCloth = document.createElement("div");
-colMenCloth.setAttribute("class", "mega-col");
-
-const hMenCloth = document.createElement("h5");
-hMenCloth.innerText = "Clothing";
-
-const ulMenCloth = document.createElement("ul");
-
-["All Cloathing", "Hoodies", "Tops & T-shirts", "Accessorieas"].forEach(text => {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-  a.href = "#";
-  a.innerText = text;
-  li.appendChild(a);
-  ulMenCloth.appendChild(li);
-});
-
-colMenCloth.append(hMenCloth, ulMenCloth);
-megaInnerMen.appendChild(colMenCloth);
-
-const colMenSport = document.createElement("div");
-colMenSport.setAttribute("class", "mega-col");
-
-const hMenSport = document.createElement("h5");
-hMenSport.innerText = "Sport";
-
-const ulMenSport = document.createElement("ul");
-
-["Running", "Football", "Basketball", "Training & Gym"].forEach(text => {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-  a.href = "#";
-  a.innerText = text;
-  li.appendChild(a);
-  ulMenSport.appendChild(li);
-});
-
-colMenSport.append(hMenSport, ulMenSport);
-megaInnerMen.appendChild(colMenSport);
-
-const liWoman = document.createElement("li");
-liWoman.setAttribute("class", "has-mega");
-
-const aWoman = document.createElement("a");
-aWoman.setAttribute("href", "#");
-aWoman.innerText = "Woman";
-
-liWoman.appendChild(aWoman);
-catalog.appendChild(liWoman);
-
-// mega
-const megaWoman = document.createElement("div");
-megaWoman.setAttribute("class", "mega");
-megaWoman.setAttribute("role", "menu");
-megaWoman.setAttribute("aria-hidden", "true");
-
-const megaInnerWoman = document.createElement("div");
-megaInnerWoman.setAttribute("class", "mega-inner");
-
-megaWoman.appendChild(megaInnerWoman);
-liWoman.appendChild(megaWoman);
-
-const colWomanShoes = document.createElement("div");
-colWomanShoes.setAttribute("class", "mega-col");
-
-const hWomanShoes = document.createElement("h5");
-hWomanShoes.innerText = "Shoes";
-
-const ulWomanShoes = document.createElement("ul");
-
-["All Shoes", "Sneakers", "Shoes", "Sport"].forEach(text => {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-  a.href = "#";
-  a.innerText = text;
-  li.appendChild(a);
-  ulWomanShoes.appendChild(li);
-});
-
-colWomanShoes.appendChild(hWomanShoes);
-colWomanShoes.appendChild(ulWomanShoes);
-megaInnerWoman.appendChild(colWomanShoes);
-
-const colWomanCloth = document.createElement("div");
-colWomanCloth.setAttribute("class", "mega-col");
-
-const hWomanCloth = document.createElement("h5");
-hWomanCloth.innerText = "Clothing";
-
-const ulWomanCloth = document.createElement("ul");
-
-["All Cloathing", "Hoodies", "Leggings", "Tops & T-shirts"].forEach(text => {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-  a.href = "#";
-  a.innerText = text;
-  li.appendChild(a);
-  ulWomanCloth.appendChild(li);
-});
-
-colWomanCloth.appendChild(hWomanCloth);
-colWomanCloth.appendChild(ulWomanCloth);
-megaInnerWoman.appendChild(colWomanCloth);
-
-const colWomanSport = document.createElement("div");
-colWomanSport.setAttribute("class", "mega-col");
-
-const hWomanSport = document.createElement("h5");
-hWomanSport.innerText = "Sport";
-
-const ulWomanSport = document.createElement("ul");
-
-["Training & Gym", "Running", "Tennis", "Football"].forEach(text => {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-  a.href = "#";
-  a.innerText = text;
-  li.appendChild(a);
-  ulWomanSport.appendChild(li);
-});
-
-colWomanSport.appendChild(hWomanSport);
-colWomanSport.appendChild(ulWomanSport);
-megaInnerWoman.appendChild(colWomanSport);
-
-const liKids = document.createElement("li");
-liKids.setAttribute("class", "has-mega");
-
-const aKids = document.createElement("a");
-aKids.setAttribute("href", "#");
-aKids.innerText = "Kids";
-
-liKids.appendChild(aKids);
-catalog.appendChild(liKids);
-
-// mega
-const megaKids = document.createElement("div");
-megaKids.setAttribute("class", "mega");
-megaKids.setAttribute("role", "menu");
-megaKids.setAttribute("aria-hidden", "true");
-
-const megaInnerKids = document.createElement("div");
-megaInnerKids.setAttribute("class", "mega-inner");
-
-megaKids.appendChild(megaInnerKids);
-liKids.appendChild(megaKids);
-
-const colKidsShoes = document.createElement("div");
-colKidsShoes.setAttribute("class", "mega-col");
-
-const hKidsShoes = document.createElement("h5");
-hKidsShoes.innerText = "Shoes";
-
-const ulKidsShoes = document.createElement("ul");
-
-["All Shoes", "Sneakers", "Shoes", "Sport"].forEach(text => {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-  a.href = "#";
-  a.innerText = text;
-  li.appendChild(a);
-  ulKidsShoes.appendChild(li);
-});
-
-colKidsShoes.appendChild(hKidsShoes);
-colKidsShoes.appendChild(ulKidsShoes);
-megaInnerKids.appendChild(colKidsShoes);
-
-const colKidsCloth = document.createElement("div");
-colKidsCloth.setAttribute("class", "mega-col");
-
-const hKidsCloth = document.createElement("h5");
-hKidsCloth.innerText = "Clothing";
-
-const ulKidsCloth = document.createElement("ul");
-
-["All Cloathing", "Tops & T-Shirts", "Tracksuits", "Jackets"].forEach(text => {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-  a.href = "#";
-  a.innerText = text;
-  li.appendChild(a);
-  ulKidsCloth.appendChild(li);
-});
-
-colKidsCloth.appendChild(hKidsCloth);
-colKidsCloth.appendChild(ulKidsCloth);
-megaInnerKids.appendChild(colKidsCloth);
-
-const colKidsSport = document.createElement("div");
-colKidsSport.setAttribute("class", "mega-col");
-
-const hKidsSport = document.createElement("h5");
-hKidsSport.innerText = "Sport";
-
-const ulKidsSport = document.createElement("ul");
-
-["Running", "Football", "Basketball", "Physical Education"].forEach(text => {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-  a.href = "#";
-  a.innerText = text;
-  li.appendChild(a);
-  ulKidsSport.appendChild(li);
-});
-
-colKidsSport.appendChild(hKidsSport);
-colKidsSport.appendChild(ulKidsSport);
-megaInnerKids.appendChild(colKidsSport);
-
 
 const navAction = document.createElement("div");
-navAction.setAttribute("class", "nav-action");
+navAction.className = "nav-action";
 nav.appendChild(navAction);
 
 const form = document.createElement("form");
-form.setAttribute("class", "search");
+form.className = "search";
 form.setAttribute("role", "search");
 
 const input = document.createElement("input");
 input.type = "search";
 input.name = "q";
 input.placeholder = "Search";
-input.setAttribute("aria-label", "Caută");
+input.setAttribute("aria-label", "Search");
 
 const btn = document.createElement("button");
 btn.type = "submit";
-btn.setAttribute("class", "search-btn");
+btn.className = "search-btn";
 
 const icon = document.createElement("span");
-icon.setAttribute("class", "material-symbols-outlined");
+icon.className = "material-symbols-outlined";
 icon.innerText = "search";
 
 btn.appendChild(icon);
@@ -329,16 +142,16 @@ navAction.appendChild(form);
 
 const themeBtn = document.createElement("button");
 themeBtn.id = "themeToggle";
-themeBtn.setAttribute("class", "theme-toggle");
+themeBtn.className = "theme-toggle";
 themeBtn.setAttribute("aria-pressed", "false");
 
 const light = document.createElement("span");
-light.setAttribute("class", "material-symbols-outlined");
+light.className = "material-symbols-outlined";
 light.id = "light";
 light.innerText = "light_mode";
 
 const dark = document.createElement("span");
-dark.setAttribute("class", "material-symbols-outlined");
+dark.className = "material-symbols-outlined";
 dark.id = "dark";
 dark.innerText = "dark_mode";
 
